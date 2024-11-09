111b77f6ec74f86f6cc987d295e8761ff924100d
AshishSK123 <ashishskharat18@gmail.com>
2024-11-10 01:39:05 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
 
   return (
     <>
-      <nav style={{border:'1px solid black'}} className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}>
+      <nav style={{border:'1px solid black'}} className={`navbar navbar-expand-lg  fixed-top navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <b className="navbar-brand">{props.title}</b>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
